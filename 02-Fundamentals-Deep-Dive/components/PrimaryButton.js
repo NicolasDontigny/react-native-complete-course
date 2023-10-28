8a8a4803e80838b85dec7c95ef6df30f5bf71336
Nicolas Dontigny <nicolas.dontigny@beslogic.com>
2023-10-27 20:54:37 -0400

    Added number form and buttons

--- 02-Fundamentals-Deep-Dive/components/PrimaryButton.js
+++ 02-Fundamentals-Deep-Dive/components/PrimaryButton.js
@@ -1,11 +1,46 @@
-import { Pressable, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
-function PrimaryButton({ children }) {
-  return <Pressable>
-    <View>
-      <Text>{children}</Text>
+function PrimaryButton({ children, onPress }) {
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) => pressed
+          ? [
+            styles.buttonInnerContainer,
+            styles.pressed
+          ]
+          : styles.buttonInnerContainer}
+        onPress={onPress}
+        android_ripple={{ color: '#640233' }}
+      >
+        <Text style={styles.text}>{children}</Text>
+      </Pressable>
     </View>
-  </Pressable>
+  )
 }
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    margin: 4,
+    borderRadius: 28,
+    overflow: 'hidden',
+    borderColor: 'orange',
+    borderWidth: 0,
+  },
+  buttonInnerContainer: {
+    backgroundColor: '#72063c',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  text: {
+    fontSize: 18,
+    color: '#ddb52f',
+    textAlign: 'center',
+  },
+  pressed: {
+    opacity: 0.75,
+  }
+})
--- 02-Fundamentals-Deep-Dive/screens/StartGameScreen.js
+++ 02-Fundamentals-Deep-Dive/screens/StartGameScreen.js
@@ -1,11 +1,42 @@
 import { TextInput, View, StyleSheet } from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
+import { useState } from 'react';
 
 function StartGameScreen() {
+  const [enteredNumber, setEnteredNumber] = useState('')
+
+  const numberInputHandler = (value) => {
+    setEnteredNumber(value)
+  }
+
+  const resetHandler = () => {
+    console.log('RESET');
+  }
+
+  const confirmHandler = () => {
+    console.log('CONFIRM:', enteredNumber);
+  }
+
   return <View style={styles.inputContainer} >
-    <TextInput />
-    <PrimaryButton>Reset</PrimaryButton>
-    <PrimaryButton>Confirm</PrimaryButton>
+    {/* Text Input value is ALWAYS of type string, no matter the keyboardType */}
+    <TextInput
+      inputMode='numeric'
+      keyboardType='number-pad'
+      autoCapitalize='none'
+      autoCorrect={false}
+      maxLength={2}
+      style={styles.numberInput}
+      onChangeText={numberInputHandler}
+      value={enteredNumber}
+    />
+    <View style={styles.buttonsContainer}>
+      <View style={styles.buttonContainer}>
+        <PrimaryButton onPress={resetHandler}>Reset</PrimaryButton>
+      </View>
+      <View style={styles.buttonContainer}>
+        <PrimaryButton onPress={confirmHandler}>Confirm</PrimaryButton>
+      </View>
+    </View>
   </View>
 }
 
@@ -13,12 +44,13 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
   inputContainer: {
-    padding: 16,
+    justifyContent: 'center',
     alignItems: 'center',
+    padding: 16,
     marginTop: 100,
     marginHorizontal: 24,
-    borderRadius: 8,
-    backgroundColor: '#72063c',
+    borderRadius: 12,
+    backgroundColor: '#3b021f',
     // Elevation is an android-specific property only
     elevation: 4,
     // Shadow properties for ios only
@@ -26,5 +58,23 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 5, height: 4 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+  },
+  numberInput: {
+    alignSelf: 'center',
+    height: 50,
+    width: 50,
+    textAlign: 'center',
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold'
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+  },
+  buttonContainer: {
+    flex: 1,
   }
 })
